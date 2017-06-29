7fe0f87a383454c710cbf5b2854c991e3863cbc6
Mike Cowgill <michael_cowgill@intuit.com>
2017-06-28 17:49:22 -0700

    this is the work around

--- modules/destination_account.tf
+++ modules/destination_account.tf
@@ -1,8 +1,10 @@
 provider "aws" {
   region = "eu-west-1"
+  alias = "local"
 }
 
 resource "aws_s3_bucket" "destination" {
+  provider = "aws.local"
   bucket   = "tf-test-bucket-destination-cowgill"
   region   = "eu-west-1"
 
